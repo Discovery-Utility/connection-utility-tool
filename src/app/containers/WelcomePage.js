6cc86daac86d92fed370ba462db2e0793b1aaad7
kseniadumpling <ksenia.dumpling@gmail.com>
2019-06-28 02:35:36 +0300

    Rewrite render method, change an appearance of the page

--- src/app/containers/WelcomePage.js
+++ src/app/containers/WelcomePage.js
@@ -3,29 +3,26 @@ import AppHeader from "../components/AppHeader";
 import t from './../locales/translation';
 import Button from './../components/Button';
 import '../../scss/pages/_welcomepage.scss'
-
 const {shell} = require('electron');
 import {Redirect} from 'react-router-dom'
+import {check_wifi, disable_wifi} from "../scripts/wifi";
+import {check_bluetooth, disable_bluetooth} from "../scripts/bluetooth";
+import {check_firewall, disable_firewall} from "../scripts/firewall";
 
+//const COUNT_SECONDS = env.SEARCH_DELAY;
 /**
  * Welcome page displays welcome message and button to scan the appliances
  */
 class WelcomePage extends Component {
     constructor(props) {
         super(props);
-        const disableNetworkPath = "http://lmgtfy.com/?q=how+to+disable+network";
-        const disableFirewallPath = "http://lmgtfy.com/?q=how+to+disable+firewall+windows+10";
-
         this.state = {
-           redirectToSearch: false
-        };
-
-        this.clickOnShowDisableNetwork = () => {
-            shell.openExternal(disableNetworkPath);
-        };
-
-        this.clickOnShowDisableFirewall = () => {
-            shell.openExternal(disableFirewallPath);
+            redirectToSearch: false,
+            redirectToErrorPage: false,
+            isWiFiEnabled: undefined,
+            isFirewallEnabled: undefined,
+            isBluetoothEnabled: undefined,
+            platform: process.platform
         };
 
         this.clickOnScanBtn = () => {
@@ -33,48 +30,122 @@ class WelcomePage extends Component {
                 redirectToSearch: true
             })
         };
+
+        this.clickOnCancelBtn = () => {
+            this.setState({
+                redirectToErrorPage: true
+            })
+        };
+        this.disableWiFi = this.disableWiFi.bind(this);
+        this.disableFirewall = this.disableFirewall.bind(this);
+        this.disableBluetooth = this.disableBluetooth.bind(this);
+        this.checkAll = this.checkAll.bind(this);
+    }
+
+    disableWiFi() {
+        disable_wifi();
+        this.setState({isWiFiEnabled: false})
+    }
+
+    disableFirewall() {
+        disable_firewall();
+        this.setState({isFirewallEnabled: false})
+    }
+
+    disableBluetooth() {
+        disable_bluetooth();
+        this.setState({isBluetoothEnabled: false})
+    }
+
+    checkAll() {
+        check_wifi().then(res => {
+            this.setState({isWiFiEnabled: res})
+        });
+        check_firewall().then(res => {
+            this.setState({isFirewallEnabled: res})
+        });
+        check_bluetooth().then(res => {
+            this.setState({isBluetoothEnabled: res})
+        });
+    };
+
+    componentDidMount() {
+        this.checkAll();
+        setInterval(() => this.checkAll(), 3000);
     }
 
     render() {
 
         let redirect = this.state.redirectToSearch;
+
+        let wifiRenderBlock = null;
+        let bluetoothRenderBlock = null;
+        let firewallRenderBlock = null;
+
+        // Check WiFi state, generate tags
+        if (this.state.isWiFiEnabled) {
+            wifiRenderBlock = <h6> Wi-Fi : <b className="red"> enabled </b>
+                                <img src={require('../../images/disable2.svg')} width="20" height="20"
+                                     className="d-inline-block pb-1"
+                                     onClick={this.disableWiFi} alt="disable"/>
+                                </h6>;
+        }
+        else {
+            wifiRenderBlock = <h6> Wi-Fi : disabled </h6>;
+        }
+
+        // Check Bluetooth state, generate tags
+        if(this.state.isBluetoothEnabled){
+            if(this.state.platform !== 'win32') {
+                bluetoothRenderBlock = <h6> Bluetooth : <b className="red"> enabled </b></h6>;
+            }
+            else {
+                bluetoothRenderBlock = (<h6> Bluetooth : <b className="red"> enabled    </b>
+                                        <img src={require('../../images/disable2.svg')} width="20" height="20"
+                                             className="d-inline-block pb-1"
+                                             onClick={this.disableBluetooth} alt="disable"/>
+                                        </h6>);
+            }
+        }
+        else {
+            bluetoothRenderBlock = <h6> Bluetooth : disabled </h6>;
+        }
+
+        // Check Firewall state, generate tags
+        if (this.state.platform === 'win32'){
+            if(this.state.isFirewallEnabled)
+                firewallRenderBlock = <h6> Firewall : <b className="red"> enabled </b> </h6>;
+            else
+                firewallRenderBlock = <h6> Firewall : disabled </h6>;
+        }
+        else {
+            firewallRenderBlock = null;
+        }
+
         return (
             <div>
                 <AppHeader/>
                 <div className="container">
-                    <div className="row justify-content-center">
-                        <div className="col-12 text-center">
-                            <h1 className="welcome-page-title">{t.WELCOME_TITLE}</h1>
+                    <div className="scroll">
+                        <div className="row justify-content-center">
+                            <div className="col-12 text-center">
+                                <h1 className="welcome-page-title">{t.WELCOME_TITLE}</h1>
+                            </div>
+                        </div>
+                        <div className="row justify-content-center">
+                            <div className="col-4 text-center">
+                                <p>{t.ABOUT_TOOL}</p>
+                            </div>
+                        </div>
+                        <div className="text-center">
+                            { wifiRenderBlock }
+                            { bluetoothRenderBlock }
+                            { firewallRenderBlock }
+                        </div>
+                        <div className="row justify-content-center">
+                            <Button available={true} text={t.SCAN_APPLIANCES} onClick={this.clickOnScanBtn}/>
                         </div>
                     </div>
-                    <div className="row justify-content-center">
-                        <div className="col-4 text-center">
-                            <p>{t.ABOUT_TOOL}</p>
-                        </div>
-                    </div>
-                    <div className="row justify-content-center">
-                        <p className="text-center"><b>1.</b> {t.CONNECT_YOUR_LAPTOP}</p>
-                    </div>
-                    <div className="row justify-content-center">
-                        <p className="show-link" onClick={this.clickOnShowDisableNetwork}>  {t.SHOW_ME_HOW}</p>
-                    </div>
-                    <div className="row justify-content-center">
-                        <p className="text-center"><b>2.</b> {t.WIFI_NETWORKS}</p>
-                    </div>
-                    <div className="row justify-content-center">
-                        <p className="show-link" onClick={this.clickOnShowDisableNetwork}>  {t.SHOW_ME_HOW}</p>
-                    </div>
-                    <div className="row justify-content-center">
-                        <p className="text-center"><b>3.</b> {t.PC_FIRE_WALL}</p>
-                    </div>
-                    <div className="row justify-content-center">
-                        <p className="show-link" onClick={this.clickOnShowDisableFirewall}>  {t.SHOW_ME_HOW}</p>
-                    </div>
-                    <div className="row justify-content-center">
-                        <Button available={true} text={t.SCAN_APPLIANCES} onClick={this.clickOnScanBtn}/>
-                    </div>
-
-                    {/*redirect to next page*/}
                     {redirect ? <Redirect to="/search"/> : null}
                 </div>
             </div>
